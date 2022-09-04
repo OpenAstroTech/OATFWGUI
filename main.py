907516b201c03958fa4b2671c5e1f62903180ed3
Julianne Swinoga <julianneswinoga@gmail.com>
2022-09-04 12:54:00 -0400

    Functionalize functionality

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import sys
 import zipfile
 import re
 from pathlib import Path
+from typing import Dict, List
 
 from PySide6.QtCore import Qt, Slot
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import *
 
 import requests
 from platformio.run.cli import cli as pio_run
@@ -21,45 +22,53 @@ class MainWidget(QWidget):
             "Hallo Welt",
         ]
 
+        # widgets
         self.button = QPushButton('Click me!')
         self.message = QLabel('Hello World')
         self.message.alignment = Qt.AlignCenter
+        self.logText = QPlainTextEdit()
+        self.logText.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.logText.setReadOnly(True)
 
-        self.layout = QVBoxLayout(self)
-        self.layout.addWidget(self.message)
-        self.layout.addWidget(self.button)
+        # layout
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(self.logText, 0, 1)
+        self.layout.addWidget(self.message, 0, 0)
+        self.layout.addWidget(self.button, 1, 0)
 
-        # Connecting the signal
+        # signals
         self.button.clicked.connect(self.magic)
 
     @Slot()
     def magic(self):
         self.message.setText(random.choice(self.hello))
+        self.logText.appendHtml('aaaaaaa' * 50)
+        self.logText.appendHtml('<b>bbbb</b>' * 50)
 
 
-def in_virtualenv():
-    return sys.base_prefix != sys.prefix
-
-
-def main():
-    if not in_virtualenv():
-        print('I should be running in a virtual environment! Something is wrong...')
-        exit(1)
-
-    response = requests.get('https://api.github.com/repos/OpenAstroTech/OpenAstroTracker-Firmware/releases')
+def get_fw_versions() -> Dict[str, str]:
+    fw_api_url = 'https://api.github.com/repos/OpenAstroTech/OpenAstroTracker-Firmware/releases'
+    print(f'Grabbing available FW versions from {fw_api_url}')
+    response = requests.get(fw_api_url)
     releases_dict = {
         'develop': 'https://github.com/OpenAstroTech/OpenAstroTracker-Firmware/archive/refs/heads/develop.zip',
     }
     for release_json in response.json():
         releases_dict[release_json['name']] = release_json['zipball_url']
+    return releases_dict
 
-    zip_url = releases_dict['Arduino V1.11.5']
+
+def download_fw(zip_url: str) -> str:
     print(f'Downloading OAT FW from: {zip_url}')
     resp = requests.get(zip_url)
     zipfile_name = 'OATFW.zip'
     with open(zipfile_name, 'wb') as fd:
         fd.write(resp.content)
         fd.close()
+    return zipfile_name
+
+
+def extract_fw(zipfile_name: str) -> str:
     print('Extracting FW')
     with zipfile.ZipFile(zipfile_name, 'r') as zip_ref:
         zip_infolist = zip_ref.infolist()
@@ -69,8 +78,11 @@ def main():
             print(f'Could not find FW top level directory in {zip_infolist}!')
             exit(1)
         zip_ref.extractall()
-    print(f'FW dir: {fw_dir}')
+    print(f'Extracted FW to {fw_dir}')
+    return fw_dir
 
+
+def get_pio_environments(fw_dir: str) -> List[str]:
     with open(Path(fw_dir, 'platformio.ini').resolve(), 'r') as fp:
         ini_lines = fp.readlines()
     environment_lines = [l for l in ini_lines if l.startswith('[env:')]
@@ -79,10 +91,25 @@ def main():
         match = re.search(r'\[env:(.+)\]', environment_line)
         if match:
             pio_environments.append(match.group(1))
-    print(pio_environments)
+    print(f'Found pio environments: {pio_environments}')
+    return pio_environments
 
-    print('Building FW')
-    pio_run(['--environment', 'mksgenlv21', '--project-dir', fw_dir])
+
+def build_fw(pio_environment: str, fw_dir: str):
+    print(f'Building FW environment={pio_environment} dir={fw_dir}')
+    pio_run(['--environment', pio_environment, '--project-dir', fw_dir])
+
+
+def main():
+    if sys.base_prefix == sys.prefix:
+        print('I should be running in a virtual environment! Something is wrong...')
+        exit(1)
+
+    releases_dict = get_fw_versions()
+    zipfile_name = download_fw(releases_dict['Arduino V1.11.5'])
+    fw_dir = extract_fw(zipfile_name)
+    pio_environments = get_pio_environments(fw_dir)
+    build_fw(pio_environments[0], fw_dir)
     exit(0)
 
     app = QApplication(sys.argv)
