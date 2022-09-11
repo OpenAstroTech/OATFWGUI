ac9b293df3267e341a2c0b9a901c1d5587c57aea
Julianne Swinoga <julianneswinoga@gmail.com>
2022-09-10 21:33:39 -0400

    Changed to .spec building

--- scripts/build_app_linux.sh
+++ scripts/build_app_linux.sh
@@ -2,14 +2,11 @@
 set -e
 
 REPO_DIR=$(git rev-parse --show-toplevel)
-MEI_BIN_DIR="bin"
 
 pushd $REPO_DIR
 ./.venv/bin/pyinstaller \
 --distpath=./dist_linux --workpath=./build_linux \
---noconfirm --onedir --clean --name=OATFWGUI \
---collect-all=platformio \
---add-binary=./.venv/bin/platformio:$MEI_BIN_DIR \
- "$@" \
-./OATFWGUI/main.py
+--noconfirm --clean \
+"$@" \
+./OATFWGUI.spec
 popd
