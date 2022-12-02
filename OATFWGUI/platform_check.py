42537181d94a58133e35a71594ab3a66853ba7cf
Julianne Swinoga <julianneswinoga@gmail.com>
2022-12-02 18:17:31 -0500

    Abstract platform check into Enum

---
+++ OATFWGUI/platform_check.py
@@ -0,0 +1,37 @@
+import enum
+import platform
+import logging
+
+platform_lookup_cache = None
+log = logging.getLogger('')
+
+
+class PlatformEnum(enum.Enum):
+    LINUX = enum.auto()
+    WINDOWS = enum.auto()
+    UNKNOWN = enum.auto()
+
+
+def get_platform() -> PlatformEnum:
+    global platform_lookup_cache
+    if platform_lookup_cache is not None:
+        # Not really for performance, but so that we can use the logger
+        # without worrying about recursion
+        return platform_lookup_cache
+
+    # No logging just yet!!
+    platform_str = platform.system().lower()
+    if 'windows' in platform_str:
+        platform_lookup = PlatformEnum.WINDOWS
+    elif 'linux' in platform_str:
+        platform_lookup = PlatformEnum.LINUX
+    else:
+        platform_lookup = PlatformEnum.UNKNOWN
+    platform_lookup_cache = platform_lookup  # Cache return
+    # We can now use logging
+
+    log.debug(f'platform_str={platform_str}')
+    if platform_lookup == PlatformEnum.UNKNOWN:
+        log.warning(f'Unknown platform {platform_str}!')
+
+    return platform_lookup
