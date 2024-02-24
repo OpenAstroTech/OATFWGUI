3bc2e36c08e6744004b4915db2ff94d719b9edff
Julianne Swinoga <julianneswinoga@gmail.com>
2024-02-24 17:46:57 -0500

    Hotfix for uploading with platform atmelavr
    https://github.com/OpenAstroTech/OpenAstroTracker-Firmware/pull/242
    will fix the latest, but that still leaves previous versions in a
    broken state, so this hotfix will probably be permanent.

--- OATFWGUI/gui_state.py
+++ OATFWGUI/gui_state.py
@@ -2,7 +2,6 @@ import logging
 from typing import NamedTuple, List, Optional
 from pathlib import Path
 
-
 log = logging.getLogger('')
 
 
@@ -30,3 +29,13 @@ class LogicState:
     def __setattr__(self, key, val):
         log.debug(f'LogicState updated: {key} {getattr(self, key)} -> {val}')
         super().__setattr__(key, val)
+
+    def env_is_avr_based(self):
+        # For stupid hot patches that only affect AVR based boards :/
+        env_not_none = self.pio_env is not None
+
+        is_avr_based = env_not_none and any(
+            avr_env_substr in self.pio_env.lower()
+            for avr_env_substr in ['mksgenl', 'ramps']
+        )
+        return is_avr_based
